5a2452798f5efac6643090842f70fa3a0141df2b
ashsajal1 <ashsajal1@gmail.com>
2023-12-21 17:18:07 +0800

    Update user model with validator

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,8 +1,21 @@
 import { prisma } from "../../prisma/client";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 //Register new user
 export const registerUser = async (email: string, password: string) => {
+
+  if(!email || !password) {
+    throw new Error("All fields must be filled.")
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error("Email is not valid.")
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough.")
+  }
   const existedUser = await prisma.user.findUnique({
     where: {
       email: email,
@@ -10,7 +23,7 @@ export const registerUser = async (email: string, password: string) => {
   });
 
   if (existedUser) {
-    throw new Error("User already exist!");
+    throw new Error("Email already in use.");
   }
 
   const salt = await bcrypt.genSalt(10);
